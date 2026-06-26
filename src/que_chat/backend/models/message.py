c0870abb2e1241496db666e8a19589bcc84eb802
W1-user <wolframgit@gmail.com>
2026-06-26 09:55:28 +0300

    Refactoring database

--- src/que_chat/backend/models/message.py
+++ src/que_chat/backend/models/message.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from sqlalchemy import Integer, String, Boolean, DateTime, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.que_chat.database import Base
+from que_chat.database import Base
 
 
 class Message(Base):
@@ -12,11 +12,11 @@ class Message(Base):
     id: Mapped[int] = mapped_column(
         Integer, primary_key=True, autoincrement=True, index=True
     )
-    sender: Mapped[str] = mapped_column(String, index=True)
+    username: Mapped[str] = mapped_column(String, nullable=False, index=True)
     text: Mapped[str] = mapped_column(
         Text,
     )
     # reaction: Mapped[str] = mapped_column(String)
-    timestamp: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow())
+    timestamp: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
